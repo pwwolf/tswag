cd31c1b7339c8738ac1ef6eba046cda3ed3e98a2
Patrick Wolf <patrick@kapokdigital.com>
2020-01-13 16:08:00 -0500

    Automatically convert swagger 2.0 to 3.0

--- src/bin.ts
+++ src/bin.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { bundle, parse, dereference } from "swagger-parser";
+import { bundle, dereference } from "swagger-parser";
 import { OpenApiSpec } from "@loopback/openapi-v3-types";
 import { generateApi } from "./lib/codegen/generator";
 import * as minimist from "minimist";
 import { stat } from "fs";
+const converter = require("swagger2openapi");
 
 const argv = minimist(process.argv.slice(2));
 
@@ -13,15 +14,29 @@ if (argv._.length < 1) {
   process.exit(0);
 }
 
-stat(argv._[0], (err, stats) => {
+stat(argv._[0], async (err, stats) => {
   if (err) {
     console.warn(`${argv._[0]} does not exist.`);
     process.exit(0);
   }
 
-  bundle(argv._[0]).then(result => {
-    dereference(result).then(deref => {
-      generateApi(result as OpenApiSpec, deref as OpenApiSpec);
+  let result = await bundle(argv._[0]);
+  if ((result as any)["swagger"]) {
+    console.warn("Converting Swagger 2.0 doc to OpenAPI 3.0");
+    let doc = await convertSwaggerToOpenApi(result);
+    result = await bundle(doc as any);
+  }
+  let deref = await dereference(result);
+  generateApi(result as OpenApiSpec, deref as OpenApiSpec);
+});
+
+async function convertSwaggerToOpenApi(doc: any) {
+  return new Promise((resolve, reject) => {
+    converter.convertObj(doc, {}, (err: Error, options: any) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(options.openapi);
     });
   });
-});
+}
